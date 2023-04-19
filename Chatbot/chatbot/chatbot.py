fac4b72e2f294d3fc8c7b53af773f71208ee0650
Tixtor710 <robinrobert20038@gmail.com>
2023-04-19 15:57:53 +0530

    hi

--- Chatbot/chatbot/chatbot.py
+++ Chatbot/chatbot/chatbot.py
@@ -2,23 +2,23 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# Define the chatbot's responses
+
 responses = {
     "hello": "Hi there!",
     "how are you": "I'm doing well, thanks for asking!",
     "what's your name": "My name is ChatBot!",
-    # Add more responses here
+    "Tell me about Tixtor?": "Tixtor is a digital content production company",
 }
 
 @app.route('/chatbot', methods=['POST'])
 def chatbot():
-    # Get the user input from the request
+    
     user_input = request.json['message']
 
-    # Get the chatbot's response
+    
     chatbot_response = responses.get(user_input.lower(), "I'm sorry, I don't understand.")
 
-    # Return the chatbot's response as a JSON object
+    
     return jsonify({'message': chatbot_response})
 
 if __name__ == '__main__':
